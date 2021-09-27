30840322a05a289907ae07ff06006d70d8b8ffd1
DaanV2 <daanverstraten@hotmail.com>
2021-09-27 14:10:17 +0200

    Fixing action

--- src/action.ts
+++ src/action.ts
@@ -1,35 +1,16 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
+import { diagnose } from "./diagnose/entry";
 
 //Start code
 try {
   // This should be a token with access to your repository scoped in as a secret.
   // The YML workflow will need to set myToken with the GitHub Secret Token
   // token: ${{ secrets.GITHUB_TOKEN }}
-  const Folder = core.getInput('folder');
-  var result = false;
+  const Folder = core.getInput("folder");
 
-  if (result) {
-    console.log('success');
-  }
-  else {
-    console.log('failure');
-    core.setFailed('no pages were created');
-  }
+  diagnose(Folder);
 
   //If failed
 } catch (error) {
-  let message: string;
-
-  if (error.message)
-    message = error.message;
-  else
-    message = JSON.stringify(error);
-
-  if (core)
-    core.setFailed(message);
-
-  else {
-    console.log(message);
-    process.exit(1);
-  }
+  core.error(error);
 }
